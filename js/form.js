29dadddbc25885b0896acf6f639784b8028f768f
ashubelwal <ashutoshbelwal89@gmail.com>
2024-01-19 11:44:09 +0530

    added website

---
+++ js/form.js
@@ -0,0 +1,77 @@
+﻿(function () {
+
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const params = Object.fromEntries(urlSearchParams.entries());
+
+  const targetNode = document.getElementsByClassName("blue-form")[0].parentElement;
+  const config = { attributes: true, childList: true, subtree: true };
+
+  let repository = params.repository;
+  let repoDropdown = document.querySelector('[id$="Repository_SelectedValue"]');
+  let hiddenFields = document.querySelectorAll('[id*="HiddenFieldComponent"]');
+
+  if (hiddenFields.length > 0) {
+    let ele = document.querySelector('[value="Submit"]');
+    if (ele.addEventListener) {
+      ele.addEventListener("click", function () { handleHiddenFields(hiddenFields) }, false); //Modern browsers
+    } else if (ele.attachEvent) {
+      ele.attachEvent('click', function () { handleHiddenFields(hiddenFields) }); //Old IE
+    }
+  }
+
+  // fix file required preventing form submission
+  const callback = function (mutationList, observer) {
+    for (const mutation of mutationList) {
+      if (mutation.type == 'childList') {
+        for (const node of mutation.addedNodes) {
+          if (node.className == 'blue-form') {
+            removeRequired();
+          }
+        }
+      }
+    }
+  };
+
+  if (targetNode) {
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
+  }
+
+
+
+  if (repoDropdown != null && repoDropdown != 'undefined') {
+    repoDropdown.value = repository;
+    repoDropdown.disabled = true;
+
+    let ele = document.querySelector('[value="Submit"]');
+    if (ele.addEventListener) {
+      ele.addEventListener("click", resetDisabled, false);  //Modern browsers
+    } else if (ele.attachEvent) {
+      ele.attachEvent('click', resetDisabled);            //Old IE
+    }
+  }
+
+  function resetDisabled() {
+    repoDropdown.disabled = false;
+  }
+
+  // save hidden fields to localstorage; retrieve them if the form fails and needs to reload
+  function handleHiddenFields(fields) {
+    for (var item of fields) {
+      let fieldNameStart = item.name.indexOf('HiddenFieldComponent');
+      let fieldName = item.name.substr(fieldNameStart, item.name.length);
+      if (item.value != "") {
+        window.localStorage[fieldName] = item.value;
+      }
+      else {
+        item.value = window.localStorage[fieldName];
+      }
+    }
+  }
+
+  function removeRequired() {
+    document.querySelector('input[type=file]').required = false;
+    document.querySelector('input[type=file]').removeAttribute('aria-required');
+  }
+
+})();
---
+++ js/recaptchav3.js
@@ -0,0 +1,14 @@
+﻿if (document.getElementById("RecaptchaV3") != null) {
+  setTimeout(VerifyForm, 1000);
+}
+
+function VerifyForm() {
+  //event.preventDefault();
+  let key = document.getElementById("RecaptchaV3").value;
+  grecaptcha.ready(function () {
+    grecaptcha.execute(key, { action: 'submit' }).then(function (token) {
+      document.getElementById('g-recaptcha-response').value = token;
+      document.getElementsByClassName("RecaptchaV3-captchaToken")[0].value = token;
+    });
+  });
+}
